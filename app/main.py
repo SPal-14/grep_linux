cd9e47b3b4c12f5d1dc5d20ca08553c2452d7010
souvik-clearspot <souvik@clearspot.ai>
2024-08-24 00:37:34 +0530

    pass 5th stage

--- app/main.py
+++ app/main.py
@@ -12,10 +12,9 @@ def match_pattern(input_line, pattern):
     elif pattern == "\\w":
         return any(c.isalnum() for c in input_line)
     elif pattern[0] == "[" and pattern[-1] == "]":
+        if pattern[1] == r"^":
+            return not any(char in pattern for char in input_line)
         return any(char in pattern[1:-1] for char in input_line)
-    #add negative character groups
-    elif pattern[0] == "[^" and pattern[-1] == "]":
-        return all(char not in pattern[2:-1] for char in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
