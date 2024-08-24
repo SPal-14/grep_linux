4cd4b6f6c3dd469ca7965482f569f459fd6917d4
souvik-clearspot <souvik@clearspot.ai>
2024-08-25 01:20:22 +0530

    12th stage

--- app/main.py
+++ app/main.py
@@ -1,153 +1,211 @@
+import os
 import sys
+from io import IOBase, StringIO
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
-specialCharactersToValueMap = {}
-back_references = []
-def get_literals_from_pattern(pattern):
-    i = 0
-    literals = []
-    while i < len(pattern):
-        if pattern[i] == "\\":
-            literals.append(pattern[i : i + 2])
-            i += 2
-        elif pattern[i] == "[":
-            end_index = pattern.find("]", i)
-            literals.append(pattern[i : i + end_index + 1])
-            literals.append(pattern[i : end_index + 1])
-            i = end_index + 1
-        elif pattern[i] == "(":
-            end_index = pattern.find(")", i)
-            literals.append(pattern[i : i + end_index + 1])
-            literals.append(pattern[i : end_index + 1])
-            i = end_index + 1
-        else:
-            literals.append(pattern[i])
-            i += 1
-    return literals
-def get_real_values(literal):
-    literal_values = []
-    if literal == ".":
-        literal_values = [chr(i) for i in range(256)]
-    elif len(literal) == 1:
-        literal_values = [literal]
-    elif literal[0] == "[":
-        if literal[1] == "^":
-            sub_literals = get_literals_from_pattern(literal[2:-1])
-            non_literal_values = []
-            for x in sub_literals:
-                non_literal_values += get_real_values(x)
-            non_literal_values = set(non_literal_values)
-            literal_values = [
-                chr(i) for i in range(256) if chr(i) not in non_literal_values
-            ]
-        else:
-            sub_literals = get_literals_from_pattern(literal[1:-1])
-            literal_values = []
-            for x in sub_literals:
-                literal_values += get_real_values(x)
-            literal_values = list(set(literal_values))
-    elif literal[0] == "(":
-        sub_groups = literal[1:-1].split("|")
-        literal_values.append("(")
-        for sub_group in sub_groups:
-            literal_values.append(
-                [
-                    get_real_values(literal)
-                    for literal in get_literals_from_pattern(sub_group)
-                ]
-            )
-        print(literal_values)
-    else:
-        literal_values = specialCharactersToValueMap.get(literal, [])
-    return literal_values
-def recursive_regex_match(input_line, input_idx, pattern, pattern_idx):
-    if pattern_idx == len(pattern):
-        return input_idx
-    if pattern[pattern_idx] == ["$"]:
-        if input_idx == len(input_line) and pattern_idx == len(pattern) - 1:
-            return input_idx
-    if input_idx == len(input_line):
-        return False
-    if pattern[pattern_idx][0] == "(":
-        for i in range(1, len(pattern[pattern_idx])):
-            subgroup_idx = recursive_regex_match(
-                input_line, input_idx, pattern[pattern_idx][i], 0
-            )
-            if subgroup_idx:
-                return recursive_regex_match(input_line, subgroup_idx, pattern, pattern_idx + 1)
-                back_references.append(input_line[input_idx:subgroup_idx])
-                to_ret = recursive_regex_match(
-                    input_line, subgroup_idx, pattern, pattern_idx + 1
-                )
-                back_references.pop()
-                return to_ret
-        return False
-    if (
-        pattern[pattern_idx][0] == "\\"
-        and len(pattern[pattern_idx]) > 1
-        and pattern[pattern_idx][1].isnumeric()
-    ):
-        group_num = int(pattern[pattern_idx][1])
-        new_pattern = [[x] for x in back_references[group_num - 1]]
-        subgroup_idx = recursive_regex_match(input_line, input_idx, new_pattern, 0)
-        if subgroup_idx:
-            return recursive_regex_match(
-                input_line, subgroup_idx, pattern, pattern_idx + 1
-            )
-        return False
-    if input_line[input_idx] in pattern[pattern_idx]:
-        if len(pattern) != pattern_idx + 1 and pattern[pattern_idx + 1] == ["+"]:
-            return recursive_regex_match(
-                input_line, input_idx + 1, pattern, pattern_idx
-            ) or recursive_regex_match(
-                input_line, input_idx + 1, pattern, pattern_idx + 2
-            )
-        if len(pattern) != pattern_idx + 1 and pattern[pattern_idx + 1] == ["?"]:
-            return recursive_regex_match(
-                input_line, input_idx, pattern, pattern_idx + 2
-            ) or recursive_regex_match(
-                input_line, input_idx + 1, pattern, pattern_idx + 2
-            )
-        return recursive_regex_match(
-            input_line, input_idx + 1, pattern, pattern_idx + 1
-        )
-    if len(pattern) != pattern_idx + 1 and pattern[pattern_idx + 1] == ["?"]:
-        return recursive_regex_match(input_line, input_idx, pattern, pattern_idx + 2)
-    return False
-def match_pattern(input_line, pattern):
-    pattern_values = [
-        get_real_values(literal) for literal in get_literals_from_pattern(pattern)
-    ]
-    if pattern_values[0] == ["^"]:
-        return recursive_regex_match(input_line, 0, pattern_values, 1)
-    else:
-        for i in range(len(input_line)):
-            if recursive_regex_match(input_line, i, pattern_values, 0):
+_is_verbose = "VERBOSE" in os.environ
+# List of subgroup matchers
+references = []
+def _d(s: str, end="\n"):
+    if not _is_verbose:
+        return
+    print(s, file=sys.stderr, end=end)
+def _literal_matcher(c: str):
+    def m(f: StringIO):
+        s = f.read(1)
+        _d(f"{s} == {c}?")
+        return s == c
+    return m
+def _group_matcher(chars: str, is_pos: bool):
+    def m(f: StringIO) -> bool:
+        s = f.read(1)
+        res = s and (s in chars) == is_pos
+        _d(f'{s} {"" if is_pos else "not "}in {chars}? {res}')
+        return res
+    return m
+def _endline_matcher():
+    def m(f: StringIO):
+        s = f.read(1)
+        _d(f'end? {s == ""}')
+        return s == "" or s == "\n"
+    return m
+def _word_matcher():
+    def m(f: StringIO):
+        s = f.read(1)
+        return s.isalnum() or s == "_"
+    return m
+def _zero_more_matcher(pm):
+    def m(f: StringIO):
+        while True:
+            pos = f.tell()
+            if not pm(f):
+                f.seek(pos)
+                break
+        return True
+    return m
+def _zero_one_matcher(pm):
+    def m(f: StringIO) -> bool:
+        pos = f.tell()
+        if not pm(f):
+            f.seek(pos)
+        return True
+    return m
+def _wildcard_matcher(f: StringIO) -> bool:
+    s = f.read(1)
+    return s != "" or s != "\n"
+class SubgroupMatcher(object):
+    matchers = []
+    def __init__(self, choices):
+        self.choices = choices
+    def __call__(self, f: StringIO) -> bool:
+        for choice in self.choices:
+            _d("try choice")
+            pos = f.tell()
+            if choice(f):
+                l = f.tell() - pos
+                f.seek(pos)
+                result = f.read(l)
+                self.matchers = [_literal_matcher(c) for c in result]
+                _d(f'choice "{result}" matches')
                 return True
+            f.seek(pos)
         return False
-    # else:
-    #     raise RuntimeError(f"Unhandled pattern: {pattern}")
+    def back_matcher(self, f: IOBase) -> bool:
+        pos = f.tell()
+        if all(m(f) for m in self.matchers):
+            return True
+        f.seek(pos)
+        return False
+def _subgroup_matcher(pf):
+    choices = []
+    while True:
+        nm = pattern_to_matcher(pf, ["|", "(", ")"])
+        c = pf.read(1)
+        if c == "|":
+            _d("add choice")
+            choices.append(nm)
+        elif c == ")":
+            _d("add choice")
+            choices.append(nm)
+            _d("finish choice")
+            break
+        elif c == "(":
+            choices.append(_subgroup_matcher(pf))
+        else:
+            raise Exception(f"Unknown char in subgroup: {c}")
+    def m(f):
+        for choice in choices:
+            _d("try choice")
+            pos = f.tell()
+            if choice(f):
+                _d("choice matches")
+                return True
+            f.seek(pos)
+        return False
+    return m
+    return SubgroupMatcher(choices)
+def _next_matcher(pf: IOBase, end):
+    pos = pf.tell()
+    c = pf.read(1)
+    if c in end:
+        pf.seek(pos)
+        return None
+    matcher = None
+    if c == "\\":
+        c = pf.read(1)
+        if c == "d":
+            matcher = lambda f: f.read(1).isdigit()
+        elif c == "w":
+            matcher = _word_matcher()
+        elif c.isdigit():
+            _d(f"back reference matcher: {c}")
+            matcher = references[int(c) - 1].back_matcher
+        else:
+            matcher = _literal_matcher(c)
+    elif c == "[":
+        c = pf.read(1)
+        is_neg = c == "^"
+        chars = "" if is_neg else c
+        while (c := pf.read(1)) != "]":
+            chars += c
+        _d(f"group matcher for: {chars}")
+        matcher = _group_matcher(chars, not is_neg)
+    elif c == "(":
+        _d("subgroup matcher")
+        matcher = _subgroup_matcher(pf)
+        references.append(matcher)
+    elif c == "$":
+        _d("end matcher")
+        matcher = _endline_matcher()
+    elif c == ".":
+        _d("wildcard matcher")
+        matcher = _wildcard_matcher
+    else:
+        _d(f"literal matcher for: {c}")
+        matcher = _literal_matcher(c)
+    # Check repeater
+    pos = pf.tell()
+    c = pf.read(1)
+    if c == "+":
+        _d("one or more matcher")
+        m = matcher
+        matcher = lambda f: m(f) and _zero_more_matcher(m)(f)
+    elif c == "*":
+        _d("zero or more matcher")
+        matcher = _zero_more_matcher(matcher)
+    elif c == "?":
+        _d("zero or one matcher")
+        matcher = _zero_one_matcher(matcher)
+    else:
+        pf.seek(pos)
+    return matcher
+def _only_start_matcher(matchers):
+    def m(f: StringIO) -> bool:
+        for m in matchers:
+            if not m(f):
+                return False
+        return True
+    return m
+def _scan_matcher(matchers):
+    def m(f: StringIO) -> bool:
+        c = "start"
+        while c:
+            pos = f.tell()
+            for m in matchers:
+                if not m(f):
+                    f.seek(pos)
+                    break
+            else:
+                return True
+            c = f.read(1)
+        return False
+    return m
+def pattern_to_matcher(pf: StringIO, end=["", "\n"]):
+    pos = pf.tell()
+    is_start = pf.read(1) == "^"
+    if not is_start:
+        pf.seek(pos)
+    matchers = []
+    for m in iter(lambda: _next_matcher(pf, end), None):
+        _d("add matcher")
+        matchers.append(m)
+    if is_start:
+        _d("_only_start_matcher")
+        return _only_start_matcher(matchers)
+    else:
+        _d("_scan_matcher")
+        return _scan_matcher(matchers)
+def match_pattern(input_line: str, pattern: str):
+    matcher = pattern_to_matcher(StringIO(pattern))
+    return matcher(StringIO(input_line))
 def main():
     pattern = sys.argv[2]
     input_line = sys.stdin.read()
-    specialCharactersToValueMap["\\d"] = [str(i) for i in range(10)]
-    specialCharactersToValueMap["\\w"] = [chr(i) for i in range(ord("a"), ord("z") + 1)]
-    specialCharactersToValueMap["\\w"] += [
-        chr(i) for i in range(ord("A"), ord("Z") + 1)
-    ]
-    specialCharactersToValueMap["\\w"] += [
-        chr(i) for i in range(ord("0"), ord("9") + 1)
-    ]
-    specialCharactersToValueMap["\\w"] += ["_"]
-    specialCharactersToValueMap["\\\\"] = ["\\"]
-    for i in range(10):
-        specialCharactersToValueMap["\\" + str(i)] = "\\" + str(i)
     if sys.argv[1] != "-E":
         print("Expected first argument to be '-E'")
         exit(1)
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
     else:
